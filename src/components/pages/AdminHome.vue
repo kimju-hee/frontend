3c8a91cf0d2cf5a86613ca4a14cb2a53d9286b36
songhyunjeong <songhyunjeong@gmail.com>
2025-07-01 16:14:53 +0900

    관리자 페이지 수정 - AI 처리 결과 페이지 추가

--- src/components/pages/AdminHome.vue
+++ src/components/pages/AdminHome.vue
@@ -6,26 +6,33 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12" md="4">
-      <v-card elevation="2" class="pa-6 d-flex flex-column align-center justify-center">
-        <v-icon size="48" color="primary">mdi-account-edit</v-icon>
-        <div class="text-h6 font-weight-bold mt-2 mb-4">작가 관리</div>
-        <v-btn color="primary" @click="goTo('author')">이동</v-btn>
-      </v-card>
-      </v-col>
-      <v-col cols="12" md="4">
-      <v-card elevation="2" class="pa-6 d-flex flex-column align-center justify-center">
-        <v-icon size="48" color="info">mdi-account-group</v-icon>
-        <div class="text-h6 font-weight-bold mt-2 mb-4">독자 관리</div>
-        <v-btn color="info" @click="goTo('user')">이동</v-btn>
+      <v-col cols="12" md="6" lg="6">
+        <v-card elevation="2" class="pa-6 d-flex flex-column align-center justify-center">
+          <v-icon size="48" color="primary">mdi-account-tie</v-icon>
+          <div class="text-h6 font-weight-bold mt-2 mb-4">작가 관리</div>
+          <v-btn color="primary" @click="goTo('author')">이동</v-btn>
         </v-card>
-        </v-col>
-      <v-col cols="12" md="4">
-      <v-card elevation="2" class="pa-6 d-flex flex-column align-center justify-center">
-        <v-icon size="48" color="success">mdi-book-open-page-variant</v-icon>
-        <div class="text-h6 font-weight-bold mt-2 mb-4">도서 관리</div>
-        <v-btn color="success" @click="goTo('book')">이동</v-btn>
-      </v-card>
+      </v-col>
+      <v-col cols="12" md="6" lg="6">
+        <v-card elevation="2" class="pa-6 d-flex flex-column align-center justify-center">
+          <v-icon size="48" color="info">mdi-account</v-icon>
+          <div class="text-h6 font-weight-bold mt-2 mb-4">독자 관리</div>
+          <v-btn color="info" @click="goTo('user')">이동</v-btn>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" lg="6">
+        <v-card elevation="2" class="pa-6 d-flex flex-column align-center justify-center">
+          <v-icon size="48" color="success">mdi-book-open-page-variant</v-icon>
+          <div class="text-h6 font-weight-bold mt-2 mb-4">도서 관리</div>
+          <v-btn color="success" @click="goTo('book')">이동</v-btn>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="6" lg="6">
+        <v-card elevation="2" class="pa-6 d-flex flex-column align-center justify-center">
+          <v-icon size="48" color="deep-purple">mdi-robot</v-icon>
+          <div class="text-h6 font-weight-bold mt-2 mb-4">AI 처리 결과 대시보드</div>
+          <v-btn color="deep-purple" @click="goTo('airesult')">이동</v-btn>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -42,6 +49,8 @@ export default {
         this.$router.push('/user-management')
       } else if (type === 'book') {
         this.$router.push('/book-management')
+      } else if (type === 'airesult') {
+        this.$router.push('/processedResults')
       }
     }
   }
