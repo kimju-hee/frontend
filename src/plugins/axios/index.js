399d793d2a1af2190b4842e516c3fd09d50b4946
songhyunjeong <songhyunjeong@gmail.com>
2025-07-01 22:08:33 +0900

    axios 설정 - 공통 baseURL 지정

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,3 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
-export default axios;
+const instance = axios.create({
+    baseURL: 'http://localhost:8088',
+    withCredentials: true
+})
+export default instance;
