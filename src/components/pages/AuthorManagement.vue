2f71e6465c70db279b0ac26c577bb6faa5890f15
songhyunjeong <songhyunjeong@gmail.com>
2025-07-03 08:51:06 +0900

    작가/독자 관리 페이지 수정

--- src/components/pages/AuthorManagement.vue
+++ src/components/pages/AuthorManagement.vue
@@ -1,141 +1,285 @@
 <template>
-    <v-container>
-        <div class="panel">
-            <div class="gs-bundle-of-buttons" style="max-height:20vh; margin-bottom: 24px;">
-                <v-btn :disabled="!selectedRow" style="margin-left: 5px;" @click="approveAuthorDialog = true" class="contrast-primary-text" small color="primary">
-                    <v-icon small class="mr-2">mdi-check-circle-outline</v-icon>작가 승인
-                </v-btn>
-                <v-dialog v-model="approveAuthorDialog" width="500">
-                    <ApproveAuthor
-                        :authorName="selectedRow?.authorName"
-                        @closeDialog="approveAuthorDialog = false"
-                        @approveAuthor="approveAuthor"
-                    />
-                </v-dialog>
-                <v-btn :disabled="!selectedRow" style="margin-left: 5px;" @click="disapproveAuthorDialog = true" class="contrast-primary-text" small color="primary">
-                    <v-icon small class="mr-2">mdi-close-circle-outline</v-icon>작가 비승인
-                </v-btn>
-                <v-dialog v-model="disapproveAuthorDialog" width="500">
-                    <DisapproveAuthor
-                        :authorName="selectedRow?.authorName"
-                        @closeDialog="disapproveAuthorDialog = false"
-                        @disapproveAuthor="disapproveAuthor"
-                    />
-                </v-dialog>
-            </div>
-            <div class="table-responsive">
-                <v-table>
-                    <thead>
-                        <tr>
-                        <th>Id</th>
-                        <th>작가명</th>
-                        <th>작가 소개</th>
-                        <th>대표작</th>
-                        <th>Email</th>
-                        <th>승인 여부</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(val, idx) in value" 
-                            @click="changeSelectedRow(val)"
-                            :key="val._links?.self?.href || idx"  
-                            :style="val === selectedRow ? 'background-color: rgb(var(--v-theme-primary), 0.2) !important;':''"
-                        >
-                            <td class="font-semibold">{{ idx + 1 }}</td>
-                            <td class="whitespace-nowrap" label="작가명">{{ val.authorName }}</td>
-                            <td class="whitespace-nowrap" label="작가 소개">{{ val.introduction }}</td>
-                            <td class="whitespace-nowrap" label="대표작">{{ val.featuredWorks }}</td>
-                            <td class="whitespace-nowrap" label="Email">{{ val.email }}</td>
-                            <td class="whitespace-nowrap" label="승인 여부">{{ val.isApprove ? '승인' : '미승인' }}</td>
-                            <v-row class="ma-0 pa-4 align-center">
-                                <v-spacer></v-spacer>
-                                <Icon style="cursor: pointer;" icon="mi:delete" @click.stop="deleteRow(val)" />
-                            </v-row>
-                        </tr>
-                    </tbody>
-                </v-table>
-            </div>
+  <v-container>
+    <div class="panel">
+      <div
+        class="gs-bundle-of-buttons"
+        style="
+          max-height: 20vh;
+          margin-bottom: 24px;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        "
+      >
+        <div>
+          <template v-if="selectedRow">
+            <v-btn
+              v-if="!selectedRow.isApprove"
+              style="margin-left: 5px"
+              @click="approveAuthorDialog = true"
+              class="contrast-primary-text"
+              small
+              color="primary"
+            >
+              <v-icon
+                small
+                class="mr-2"
+                >mdi-check-circle-outline</v-icon
+              >
+              작가 승인
+            </v-btn>
+            <v-dialog
+              v-if="!selectedRow.isApprove"
+              v-model="approveAuthorDialog"
+              width="500"
+            >
+              <ApproveAuthor
+                :authorName="selectedRow?.authorName"
+                @closeDialog="approveAuthorDialog = false"
+                @approveAuthor="approveAuthor"
+              />
+            </v-dialog>
+
+            <v-btn
+              v-if="selectedRow.isApprove"
+              style="margin-left: 5px"
+              @click="disapproveAuthorDialog = true"
+              class="contrast-primary-text"
+              small
+              color="primary"
+            >
+              <v-icon
+                small
+                class="mr-2"
+                >mdi-close-circle-outline</v-icon
+              >
+              작가 비승인
+            </v-btn>
+            <v-dialog
+              v-if="selectedRow.isApprove"
+              v-model="disapproveAuthorDialog"
+              width="500"
+            >
+              <DisapproveAuthor
+                :authorName="selectedRow?.authorName"
+                @closeDialog="disapproveAuthorDialog = false"
+                @disapproveAuthor="disapproveAuthor"
+              />
+            </v-dialog>
+          </template>
+          <template v-else>
+            <v-btn style="visibility: hidden; margin-left: 5px">
+              <v-icon
+                small
+                class="mr-2"
+                >mdi-check-circle-outline</v-icon
+              >
+              작가 승인
+            </v-btn>
+            <v-btn style="visibility: hidden; margin-left: 5px">
+              <v-icon
+                small
+                class="mr-2"
+                >mdi-close-circle-outline</v-icon
+              >
+              작가 비승인
+            </v-btn>
+          </template>
         </div>
-    </v-container>
+        <div>
+          <v-btn
+            v-if="selectedRow"
+            variant="plain"
+            style="margin-left: 8px"
+            @click="deleteAuthor(selectedRow)"
+            color="secondary"
+          >
+            <v-icon
+              small
+              class="mr-2"
+              >mdi-delete</v-icon
+            >
+            작가 삭제
+          </v-btn>
+          <v-btn
+            v-else
+            variant="plain"
+            style="visibility: hidden; margin-left: 8px"
+            color="secondary"
+          >
+            <v-icon
+              small
+              class="mr-2"
+              >mdi-delete</v-icon
+            >
+            작가 삭제
+          </v-btn>
+        </div>
+      </div>
+      <div class="table-responsive">
+        <VDataTable
+          :headers="headers"
+          :items="authors"
+          item-key="id"
+          :items-per-page="10"
+          class="elevation-1"
+          @click:row="changeSelectedRow"
+        >
+          <template #item="{ item }">
+            <tr
+              :class="{ 'selected-row-class': item === selectedRow }"
+              @click="changeSelectedRow(item)"
+            >
+              <td class="font-semibold">{{ item.id }}</td>
+              <td class="whitespace-nowrap">{{ item.authorName }}</td>
+              <td class="whitespace-nowrap">{{ item.introduction }}</td>
+              <td class="whitespace-nowrap">{{ item.featuredWorks }}</td>
+              <td class="whitespace-nowrap">{{ item.email }}</td>
+              <td class="whitespace-nowrap">{{ item.isApprove ? '⭕' : '❌' }}</td>
+            </tr>
+          </template>
+        </VDataTable>
+      </div>
+    </div>
+    <v-snackbar
+      v-model="snackbar"
+      :color="snackbarColor"
+      timeout="3000"
+      location="top"
+    >
+      {{ snackbarMessage }}
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import axios from '@/plugins/axios';
-import BaseRepository from '@/components/repository/BaseRepository.js';
-import ApproveAuthor from '../ApproveAuthor.vue';
-import DisapproveAuthor from '../DisapproveAuthor.vue';
+import BaseRepository from '@/components/repository/BaseRepository.js'
+import axios from '@/plugins/axios'
+import { VDataTable } from 'vuetify/labs/VDataTable'
+import ApproveAuthor from '../ApproveAuthor.vue'
+import DisapproveAuthor from '../DisapproveAuthor.vue'
 
 export default {
-    name: 'AuthorManagement',
-    components: {
-        ApproveAuthor,
-        DisapproveAuthor,
+  name: 'AuthorManagement',
+  components: {
+    ApproveAuthor,
+    DisapproveAuthor,
+    VDataTable,
+  },
+  data() {
+    return {
+      path: 'authors',
+      repository: null,
+      authors: [
+        {
+          id: 11,
+          authorName: '홍길동',
+          introduction: '테스트용 작가입니다.',
+          featuredWorks: '가짜 소설',
+          email: 'hong-a@test.com',
+          isApprove: false,
+          _links: { self: { href: 'dummy/1' } },
+        },
+      ],
+      selectedRow: null,
+      approveAuthorDialog: false,
+      disapproveAuthorDialog: false,
+      snackbar: false,
+      snackbarMessage: '',
+      snackbarColor: 'error',
+      headers: [
+        { title: 'ID', key: 'id', sortable: true },
+        { title: '작가명', key: 'authorName', sortable: true },
+        { title: '소개말', key: 'introduction', sortable: false },
+        { title: '대표작', key: 'featuredWorks', sortable: false },
+        { title: 'Email', key: 'email', sortable: false },
+        { title: '승인 여부', key: 'isApprove', sortable: true },
+      ],
+    }
+  },
+  async created() {
+    this.repository = new BaseRepository(axios, this.path)
+    await this.fetchAuthors()
+  },
+  methods: {
+    async fetchAuthors() {
+      try {
+        this.authors = await this.repository.find()
+      } catch (e) {
+        this.showError('작가 목록을 불러오지 못했습니다.', e)
+      }
     },
-    data() {
-        return {
-            path: 'authors',
-            repository: null,
-            value: [],
-            selectedRow: null,
-            approveAuthorDialog: false,
-            disapproveAuthorDialog: false,
-        };
+    changeSelectedRow(item) {
+      if (this.selectedRow === item) {
+        this.selectedRow = null
+      } else {
+        this.selectedRow = item
+      }
     },
-    async created() {
-        this.repository = new BaseRepository(axios, this.path);
-        await this.fetchAuthors();
+    async approveAuthor(params) {
+      if (!this.selectedRow) return
+      try {
+        const path = 'approveauthor'
+        const temp = await this.repository.invoke(this.selectedRow, path, params)
+        this.updateRow(temp.data)
+        this.approveAuthorDialog = false
+        this.selectedRow = null
+        this.showSuccess('작가 승인 완료!')
+      } catch (e) {
+        this.showError('작가 승인에 실패했습니다.', e)
+      }
     },
-    methods: {
-        async fetchAuthors() {
-            try {
-                this.value = await this.repository.find();
-            } catch (e) {
-                // 에러 처리 필요시 여기에 추가
-            }
-        },
-        changeSelectedRow(val) {
-            this.selectedRow = val;
-        },
-        async approveAuthor(params) {
-            if (!this.selectedRow) return;
-            try {
-                const path = 'approveAuthor';
-                const temp = await this.repository.invoke(this.selectedRow, path, params);
-                this.updateRow(temp.data);
-                this.approveAuthorDialog = false;
-            } catch (e) {
-                // 에러 처리 필요시 여기에 추가
-            }
-        },
-        async disapproveAuthor(params) {
-            if (!this.selectedRow) return;
-            try {
-                const path = 'disapproveAuthor';
-                const temp = await this.repository.invoke(this.selectedRow, path, params);
-                this.updateRow(temp.data);
-                this.disapproveAuthorDialog = false;
-            } catch (e) {
-                // 에러 처리 필요시 여기에 추가
-            }
-        },
-        async deleteRow(row) {
-            if (!row) return;
-            try {
-                await this.repository.delete(row);
-                this.value = this.value.filter(v => v !== row);
-                this.selectedRow = null;
-                // 성공 처리 필요시 여기에 추가
-            } catch (e) {
-                // 에러 처리 필요시 여기에 추가
-            }
-        },
-        updateRow(newData) {
-            const idx = this.value.findIndex(v => v._links?.self?.href === newData._links?.self?.href);
-            if (idx !== -1) {
-                this.value[idx] = newData;
-            }
-        },
+    async disapproveAuthor(params) {
+      if (!this.selectedRow) return
+      try {
+        const path = 'disapproveauthor'
+        const temp = await this.repository.invoke(this.selectedRow, path, params)
+        this.updateRow(temp.data)
+        this.disapproveAuthorDialog = false
+        this.selectedRow = null
+        this.showSuccess('작가 비승인 완료!')
+      } catch (e) {
+        this.showError('작가 비승인에 실패했습니다.', e)
+      }
     },
-};
+    async deleteAuthor(row) {
+      if (!row) return
+      try {
+        await this.repository.delete(row)
+        this.authors = this.authors.filter(v => v !== row)
+        this.selectedRow = null
+        this.showSuccess('삭제 완료!')
+      } catch (e) {
+        this.showError('삭제에 실패했습니다.', e)
+      }
+    },
+    updateRow(newData) {
+      const idx = this.authors.findIndex(v => v._links?.self?.href === newData._links?.self?.href)
+      if (idx !== -1) {
+        this.authors[idx] = newData
+      }
+    },
+    showError(message, error) {
+      this.snackbarMessage = message
+      this.snackbarColor = 'error'
+      this.snackbar = true
+      if (error) {
+        console.error(message, error)
+      } else {
+        console.error(message)
+      }
+    },
+    showSuccess(message) {
+      this.snackbarMessage = message
+      this.snackbarColor = 'success'
+      this.snackbar = true
+    },
+  },
+}
 </script>
+
+<style scoped>
+.selected-row-class {
+  background-color: rgb(var(--v-theme-primary), 0.1) !important;
+}
+</style>
--- src/components/pages/UserManagement.vue
+++ src/components/pages/UserManagement.vue
@@ -1,131 +1,193 @@
 <template>
-    <v-container>
-        <v-snackbar
-            v-model="snackbar.status"
-            :timeout="snackbar.timeout"
-            :color="snackbar.color"
-        >
-            {{ snackbar.text }}
-            <v-btn style="margin-left: 80px;" text @click="snackbar.status = false">
-                Close
-            </v-btn>
-        </v-snackbar>
-        <div class="panel">
-            <div class="mb-5 text-lg font-bold"></div>
-            <div class="table-responsive">
-                <VDataTable
-                    :headers="headers"
-                    :items="usersWithPoints"
-                    item-key="id"
-                    :items-per-page="10"
-                    class="elevation-1"
-                    @click:row="changeSelectedRow"
-                >
-                    <template #item="{ item, index }">
-                        <tr :class="{ 'selected-row-class': item === selectedRow }" @click="changeSelectedRow(item)">
-                            <td class="font-semibold">{{ index + 1 }}</td>
-                            <td class="whitespace-nowrap">{{ item.userName }}</td>
-                            <td class="whitespace-nowrap">{{ item.point || 0 }}</td>
-                            <td class="whitespace-nowrap">{{ item.isPurchase }}</td>
-                            <td class="whitespace-nowrap">{{ item.email }}</td>
-                            <td class="whitespace-nowrap">{{ item.message }}</td>
-                        </tr>
-                    </template>
-                </VDataTable>
-            </div>
+  <v-container>
+    <div class="panel">
+      <div
+        class="gs-bundle-of-buttons"
+        style="
+          max-height: 20vh;
+          margin-bottom: 24px;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        "
+      >
+        <div>
+          <!-- 왼쪽 영역 (현재는 비어있음) -->
         </div>
-    </v-container>
+        <div>
+          <template v-if="selectedRow">
+            <v-btn
+              variant="plain"
+              style="margin-left: 8px"
+              @click="deleteUser(selectedRow)"
+              color="secondary"
+            >
+              <v-icon
+                small
+                class="mr-2"
+                >mdi-delete</v-icon
+              >
+              독자 삭제
+            </v-btn>
+          </template>
+          <template v-else>
+            <v-btn
+              variant="plain"
+              style="visibility: hidden; margin-left: 8px"
+              color="secondary"
+            >
+              <v-icon
+                small
+                class="mr-2"
+                >mdi-delete</v-icon
+              >
+              독자 삭제
+            </v-btn>
+          </template>
+        </div>
+      </div>
+      <div class="table-responsive">
+        <VDataTable
+          :headers="headers"
+          :items="usersWithPoints"
+          item-key="id"
+          :items-per-page="10"
+          class="elevation-1"
+          @click:row="changeSelectedRow"
+        >
+          <template #item="{ item }">
+            <tr
+              :class="{ 'selected-row-class': item === selectedRow }"
+              @click="changeSelectedRow(item)"
+            >
+              <td class="font-semibold">{{ item.id }}</td>
+              <td class="whitespace-nowrap">{{ item.userName }}</td>
+              <td class="whitespace-nowrap">{{ item.point || 0 }}</td>
+              <td class="whitespace-nowrap">{{ item.isPurchase ? '⭕' : '❌' }}</td>
+              <td class="whitespace-nowrap">{{ item.email }}</td>
+              <td class="whitespace-nowrap">{{ item.message }}</td>
+            </tr>
+          </template>
+        </VDataTable>
+      </div>
+    </div>
+    <v-snackbar
+      v-model="snackbar"
+      :color="snackbarColor"
+      timeout="3000"
+      location="top"
+    >
+      {{ snackbarMessage }}
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
-import axios from '@/plugins/axios';
+import BaseRepository from '@/components/repository/BaseRepository.js'
+import axios from '@/plugins/axios'
 import { VDataTable } from 'vuetify/labs/VDataTable'
 
 export default {
-    name: 'UserManagement',
-    components: {
-        VDataTable,
-    },
-    data() {
-        return {
-            path: '/users',
-            pointPath: '/points/userId',
-            users: [], // 사용자 목록
-            usersWithPoints: [], // 포인트 정보가 추가된 사용자 목록
-            selectedRow: null,
-            snackbar: {
-                status: false,
-                timeout: 5000,
-                text: '',
-                color: 'info',
-            },
-            headers: [
-                { title: 'No.', key: 'id', sortable: false },
-                { title: '독자명', key: 'userName' },
-                { title: '보유 포인트', key: 'point' },
-                { title: '월정액 구독 여부', key: 'isPurchase' },
-                { title: 'Email', key: 'email' },
-                { title: '알림 메시지', key: 'message' },
-            ],
-        };
-    },
-    created() {
-        this.fetchUsersAndPoints();
-    },
-    methods: {
-        async fetchUsersAndPoints() {
-            try {
-                const userResponse = await axios.get(this.path);
-                // Spring Data REST 응답 구조에 맞게 _embedded.users 추출
-                this.users = userResponse.data._embedded.users || [];
-
-                const pointPromises = this.users.map(async user => {
-                    try {
-                        // user.id는 Long 타입이므로 String으로 변환하여 API에 전달
-                        const pointResponse = await axios.get(`${this.pointPath}/${String(user.id)}`); // user.id를 String으로 변환
-                        return { ...user, point: pointResponse.data.point }; // 기존 사용자 정보에 point 추가
-                    } catch (pointError) {
-                        console.warn(`[UserManagement] Point not found for user ID: ${user.id}`, pointError);
-                        return { ...user, point: 0 }; // 포인트가 없을 경우 0으로 처리
-                    }
-                });
-                this.usersWithPoints = await Promise.all(pointPromises); // 모든 포인트 조회 요청이 완료될 때까지 기다림
-
-                // 성공 메시지 표시 (선택 사항)
-                this.snackbar.text = '사용자 및 포인트 정보를 성공적으로 불러왔습니다.';
-                this.snackbar.color = 'success';
-                this.snackbar.status = true;
-
-            } catch (e) {
-                // 최상위 catch 블록에서 에러 스낵바 호출
-                this.showError(e);
-            }
+  name: 'UserManagement',
+  components: {
+    VDataTable,
+  },
+  data() {
+    return {
+      path: 'users',
+      pointPath: '/points/userId',
+      repository: null,
+      usersWithPoints: [
+        {
+          id: 11,
+          userName: '홍길동',
+          point: 1000,
+          isPurchase: false,
+          email: 'hong-r@test.com',
+          message: '환영합니다!',
         },
-        // @click:row 이벤트는 item과 row 두 인자를 전달합니다.
-        changeSelectedRow(event, { item }) { // event는 돔 이벤트, item은 클릭된 아이템 데이터
-            // 이미 선택된 행을 다시 클릭하면 선택 해제
-            if (this.selectedRow === item) {
-                this.selectedRow = null;
-            } else {
-                this.selectedRow = item;
-            }
-        },
-        showError(e) {
-            this.snackbar.status = true;
-            this.snackbar.color = 'error';
-            if (e.response && e.response.data && e.response.data.message) {
-                this.snackbar.text = e.response.data.message;
-            } else {
-                this.snackbar.text = e.message || '알 수 없는 오류가 발생했습니다.';
-            }
-        },
+      ],
+      selectedRow: null,
+      snackbar: false,
+      snackbarMessage: '',
+      snackbarColor: 'error',
+      headers: [
+        { title: 'ID', key: 'id', sortable: true },
+        { title: '독자명', key: 'userName', sortable: true },
+        { title: '보유 포인트', key: 'point', sortable: true },
+        { title: '월정액 구독 여부', key: 'isPurchase', sortable: true },
+        { title: 'Email', key: 'email', sortable: false },
+        { title: '알림 메시지', key: 'message', sortable: false },
+      ],
+    }
+  },
+  async created() {
+    this.repository = new BaseRepository(axios, this.path)
+    await this.fetchUsersAndPoints()
+  },
+  methods: {
+    async fetchUsersAndPoints() {
+      try {
+        const users = await this.repository.find()
+        const usersWithPointsPromises = users.map(async user => {
+          let pointData = 0
+          try {
+            const res = await axios.get(`${this.pointPath}/${String(user.id)}`)
+            pointData = res.data.point
+          } catch (e) {
+            console.warn(`Point not found for user ID ${user.id}:`, e)
+            pointData = 0
+          }
+          return {
+            ...user,
+            point: pointData,
+          }
+        })
+        this.usersWithPoints = await Promise.all(usersWithPointsPromises)
+        this.usersWithPoints.sort((a, b) => a.id - b.id)
+      } catch (e) {
+        this.showError('독자 목록을 불러오지 못했습니다.', e)
+      }
     },
-};
+    changeSelectedRow(item) {
+      if (this.selectedRow === item) {
+        this.selectedRow = null
+      } else {
+        this.selectedRow = item
+      }
+    },
+    async deleteUser(row) {
+      if (!row) return
+      try {
+        await this.repository.delete(row)
+        this.usersWithPoints = this.usersWithPoints.filter(user => user.id !== row.id)
+        this.selectedRow = null
+        this.showSuccess('삭제 완료!')
+      } catch (e) {
+        this.showError('삭제에 실패했습니다.', e)
+      }
+    },
+    showError(message, error) {
+      this.snackbarMessage = message
+      this.snackbarColor = 'error'
+      this.snackbar = true
+      if (error) {
+        console.error(message, error)
+      } else {
+        console.error(message)
+      }
+    },
+    showSuccess(message) {
+      this.snackbarMessage = message
+      this.snackbarColor = 'success'
+      this.snackbar = true
+    },
+  },
+}
 </script>
 
 <style scoped>
-/* 선택된 행의 배경색을 위한 CSS 클래스 */
 .selected-row-class {
-    background-color: rgb(var(--v-theme-primary), 0.2) !important;
+  background-color: rgb(var(--v-theme-primary), 0.1) !important;
 }
 </style>
