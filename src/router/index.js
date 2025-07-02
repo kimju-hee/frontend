41c49c89c8c41c3a399680c2435c21a7496a2db1
songhyunjeong <songhyunjeong@gmail.com>
2025-07-03 08:41:25 +0900

    루트 경로 변경 - 로그인 페이지로 리다이렉트 적용

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      component: () => import('../components/pages/Index.vue'),
+      redirect: '/login',
     },
     {
       path: '/authors',
